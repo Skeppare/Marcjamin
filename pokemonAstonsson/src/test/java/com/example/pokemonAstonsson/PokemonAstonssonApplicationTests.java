652250368857c07fd164990a2c28121c0942329b
Skeppare <grevesnabla@gmail.com>
2025-01-23 21:38:02 +0100

    new project

---
+++ pokemonAstonsson/src/test/java/com/example/pokemonAstonsson/PokemonAstonssonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemonAstonsson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonAstonssonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
